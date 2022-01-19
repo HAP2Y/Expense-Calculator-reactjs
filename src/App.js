6c8e4b3dd39536f2c9c6ee3fef5f71d851003d12
HAP2Y <phappy1422@gmail.com>
2022-01-18 23:46:59 -0500

    adding dark mode 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { Balance } from './components/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses';
-import { TransactionList } from './components/TransactionList';
-import { AddTransaction } from './components/AddTransaction';
+import React, { useState } from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
-import { GlobalProvider } from './context/GlobalState';
+import { GlobalProvider } from "./context/GlobalState";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [toggle, SetToggle] = useState(true);
+
+  const toggleMode = () => {
+    SetToggle(!toggle);
+  };
+
   return (
     <GlobalProvider>
+      <div className="icon" onClick={toggleMode}>
+        {toggle ? <i class="fas fa-sun"></i> : <i class="fas fa-moon"></i>}
+      </div>
       <Header />
       <div className="container">
         <Balance />
